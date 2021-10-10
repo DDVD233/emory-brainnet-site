5d4f7bea0ef5f87fd3923e98bd35b3e00310e0e7
zjdavid <zjdavid.2003@gmail.com>
2021-10-10 18:17:01 -0400

    Add datasets.js

---
+++ src/pages/datasets.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
+const IndexPage = ({ data }) => {
+    return (
+        <Box component="article">
+            <MDXRenderer>
+                {data.mdx.body}
+            </MDXRenderer>
+        </Box>
+    )
+}
+
+export default IndexPage
+export const pageQuery = graphql`
+    query IndexQuery {
+        mdx(fileAbsolutePath: {regex: "/posts/datasets.mdx/"}) {
+            body
+        }
+    }
+`
