5ba80322bc6c7e6e9f9965a5b1b64aef795061f9
himself65 <himself65@outlook.com>
2021-10-10 17:42:29 -0500

    fix: markdown render

--- src/pages/datasets.js
+++ src/pages/datasets.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const IndexPage = ({ data }) => {
-    return (
-        <Box component="article">
-            <MDXRenderer>
-                {data.mdx.body}
-            </MDXRenderer>
-        </Box>
-    )
+  return (
+    <Box component="article">
+      <html dangerouslySetInnerHTML={{
+        __html: data.markdownRemark.html
+      }}/>
+    </Box>
+  )
 }
 
 export default IndexPage
 export const pageQuery = graphql`
     query DatasetsPageQuery {
-        mdx(fileAbsolutePath: {regex: "/posts/datasets.mdx/"}) {
-            body
+        markdownRemark(fileAbsolutePath: {regex: "/posts/datasets.md$/"}) {
+            html
         }
     }
 `
