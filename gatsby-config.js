017f5a90da8d70f5c7f7cc0537ecb6db4d272cc1
himself65 <himself65@outlook.com>
2021-10-10 14:26:19 -0500

    fix: disable google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
