418ca8f45f2474e536f5ce7fdd8452c463590335
zjdavid <zjdavid.2003@gmail.com>
2022-03-24 12:00:44 -0400

    Add logo

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container, Divider, Grow, Grid } from '@mui/material'
 import PersonCard from '../components/PersonCard'
+import CbisLogo from '../images/CBIS_logo_blue.png'
+import CsLogo from '../images/logo_cs_blue.png'
 
 const TeamPage = () => {
   return (
@@ -12,13 +14,12 @@ const TeamPage = () => {
             flexDirection: 'column',
         }
     }>
-        <h1 style={{padding: '20px'}}>Team</h1>
+        <h1 style={{padding: '40px'}}>Team</h1>
         {/*  Horizontal align PersonCard */}
 
         <Divider style={
             {
                 width: '100%',
-                marginTop: '50px',
                 marginBottom: '50px',
             }
         }/>
@@ -111,6 +112,29 @@ const TeamPage = () => {
                 />
             </Grid>
         </Grow>
+
+        <h1 style={{ margin: '50px' }}>Support</h1>
+
+        <Divider style={
+            {
+                width: '100%',
+                marginBottom: '30px',
+            }
+        }/>
+
+        <Container container
+                   style={{ marginTop: '50px', justifyContent: 'center', display: 'flex' }}>
+            <Container item xs={8} sm={4}>
+                <a href={'https://www.cs.emory.edu/home/'}>
+                    <img src={CsLogo} alt="CS"/>
+                </a>
+            </Container>
+            <Container item xs={8} sm={3}>
+                <a href={'http://web1.sph.emory.edu/bios/CBIS/index.html'}>
+                    <img src={CbisLogo} alt="CBIS"/>
+                </a>
+            </Container>
+        </Container>
 
     </Container>
   )
